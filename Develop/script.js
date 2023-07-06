201cce5362b201f139356a75cc4f37178029f878
Kevin-Stephens <kmstephens437@gmail.com>
2023-07-05 21:38:52 -0500

    added comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,25 @@
+//Setting global variables
+
 var confirmLength;
 var confirmLowercase;
 var confirmUppercase;
 var confirmNumeric;
 var confirmSpecial;
 
+//setting variables for PW generation
+
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var special = ["!","@","#","$","%","^","&","*"];
 var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   confirmLength = prompt("Please choose PW length between 8 and 128 characters");
-
+//set early return if confirmLength is false and move on to confirms if valid
   if (!confirmLength) {
     window.alert("Please enter a valid response")
     return
@@ -28,7 +32,7 @@ function writePassword() {
   confirmNumeric = confirm("Would you like to include numbers?");
   confirmSpecial = confirm("Would you like to include special characters?");
   };
-
+// check to make sure at least one item was set for criteria
   if (!confirmLowercase && !confirmUppercase && !confirmNumeric && !confirmSpecial) {
     window.alert("At least one criteria must be selected!");
     return
